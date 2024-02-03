155bdc018c7be09b993cc6c6e981061c78d1e004
ANISH <anishku1686@gmail.com>
2024-02-03 17:11:39 +0530

    uurdg

---
+++ break & continue.py
@@ -0,0 +1,9 @@
+#for a in range(5):
+    #if a>3:
+       # break      # break keyword is used for break the loop from moving ahead.
+   # print(a)
+
+for b in range(8):
+    if b == 5:
+         continue # so continue is used as to skip that perticlar line and move forword.
+    print(b)
