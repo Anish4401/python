559ce5e0c9b4a9794d93f7e73c0ac370e981ec5f
ANISH <anishku1686@gmail.com>
2024-02-03 17:00:25 +0530

    while loop added

--- looping.py
+++ looping.py
@@ -8,5 +8,11 @@ a=list(range(4,10)) #starting number is inclusive & last is exclusive
  
  # now in case of string
 a=['anish','manish','samir']
-for name in a:
-        print(name*3)
+#for name in a:
+       # print(name*3)
+    
+#while loop
+n=5
+while n>0:        #so here we have while loop where we prefer to use this loop only when where we dont know about how many times the loop will be runing.
+        print(n)
+        n-=1
