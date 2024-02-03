904397290dbe59b2dc0350ba5d1f6767c118f87d
ANISH <anishku1686@gmail.com>
2024-02-03 16:50:18 +0530

    looping in for loop

---
+++ looping.py
@@ -0,0 +1,12 @@
+#range it is the block that contain certain amount of space on it.
+a=list(range(4,10)) #starting number is inclusive & last is exclusive 
+#print(a)
+
+#for loop
+#for x in range(10):
+    #print(x*3,end =" ")
+ 
+ # now in case of string
+a=['anish','manish','samir']
+for name in a:
+        print(name*3)
